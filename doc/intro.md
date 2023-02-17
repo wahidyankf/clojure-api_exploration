c65a891fc14201659057eb17e96f1e19b135dc8c
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-02-17 13:34:31 +0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-public_api_exploration
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_public_api_exploration/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-public-api-exploration.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-public-api-exploration.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
