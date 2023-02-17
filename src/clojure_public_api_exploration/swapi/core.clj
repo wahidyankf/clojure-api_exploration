ae6d5ae6f73c345cbe3e923a366bb2d5d84a5277
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-02-17 13:37:35 +0700

    add swapi exploration

---
+++ src/clojure_public_api_exploration/swapi/core.clj
@@ -0,0 +1,34 @@
+(ns clojure-public-api-exploration.swapi.core
+  (:require [clojure.data.json :as json]
+            [clojure.string :as string]
+            [clj-http.client :as client]))
+
+(defn get-sw-char-movies [sw-char-id]
+  (let [sw-char (try (-> (str "https://swapi.dev/api/people/" sw-char-id)
+                         client/get
+                         :body
+                         json/read-str)
+                     (catch Exception e
+                       (let [status (-> (ex-data e)
+                                        :status)]
+                         (case status
+                           404 nil
+                           nil))))
+        movies  (if (nil? sw-char)
+                  nil
+                  (doall
+                   (->> (get sw-char "films")
+                        (map #(future (-> % client/get)))
+                        (map deref)
+                        (map (fn [res]
+                               (->> res
+                                    :body
+                                    json/read-str)))
+                        (map #(get % "title")))))]
+    (if (nil? sw-char) nil (str (get sw-char "name") " was featured in: " (string/join ", " movies) "."))))
+(get-sw-char-movies 100)
+;; => nil
+(get-sw-char-movies 1)
+;; => "Luke Skywalker was featured in: A New Hope, The Empire Strikes Back, Return of the Jedi, Revenge of the Sith."
+(get-sw-char-movies 20)
+;; => "Yoda was featured in: The Empire Strikes Back, Return of the Jedi, The Phantom Menace, Attack of the Clones, Revenge of the Sith."
